d9db4ca81755400fb51d04ebac06d925bfdc2402
tamas.markovics <markovics.ttamas@gmail.com>
2019-09-11 11:52:31 +0200

    elsö c++ feladatok

---
+++ week-01/day-3/cpp1.fel/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main() {
+
+    //HelloME
+    std::cout << "Hello T" << std::endl<<std::endl;
+
+    //HumpyTrumpy
+    std::cout << "Humpty Dumpty sat on a wall," << std::endl;
+    std::cout << "Humpty Dumpty had a great fall." << std::endl;
+    std::cout << "All the king's horses and all the king's men" << std::endl;
+    std::cout << "Couldn't put Humpty together again." << std::endl<< std::endl;
+
+    //HelloOthers
+    std::cout << "Hello, A!" << std::endl;
+    std::cout << "Hello B!" << std::endl;
+    std::cout << "Hello C!" << std::endl<<std::endl;
+
+
+
+    return 0;
+}
