662c92e5a9f2e6d14889cb2e03f065df63fa76de
tamas.markovics <markovics.ttamas@gmail.com>
2019-09-12 13:20:24 +0200

    draw chess table

--- week-01/day-4/draw_chess_table/main.cpp
+++ week-01/day-4/draw_chess_table/main.cpp
@@ -1,6 +1,39 @@
 #include <iostream>
 
 int main() {
-
+    // Crate a program that draws a chess table like this
+    //
+    // % % % %
+    //  % % % %
+    // % % % %
+    //  % % % %
+    // % % % %
+    //  % % % %
+    // % % % %
+    //  % % % %
+    std::cout << "How big would you like? Height first pls ty :)" << std::endl;
+    int wid = 0;
+    int hei = 0;
+    std::cin >> hei;
+    std::cout << "Now width" << std:: endl;
+    std::cin >> wid;
+    for (int i = 1; i <= hei; i++) {
+        if (i % 2 == 0) {
+            for (int p=1; p <= wid; p++) {
+                if (p % 2 != 0) {
+                    std::cout << " ";
+                } else
+                    std::cout << "%";
+            }
+        } else {
+            for (int p=1; p <= wid; p++) {
+                if (p % 2 == 0) {
+                    std::cout << " ";
+                } else
+                    std::cout << "%";
+            }
+        }
+        std::cout << std::endl;
+    }
     return 0;
 }
