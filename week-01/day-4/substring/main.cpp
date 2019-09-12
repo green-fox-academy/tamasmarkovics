a6e8da546cc7fb130d17af7c1ec6d824d6721861
tamas.markovics <markovics.ttamas@gmail.com>
2019-09-12 21:27:16 +0200

    add substring project to day-4

--- week-01/day-4/substring/main.cpp
+++ week-01/day-4/substring/main.cpp
@@ -1,24 +1,29 @@
 #include<iostream>
-
+#include <string>
+using namespace std;
 //  Create a function that takes two strings as a parameter
 //  Returns the starting index where the second one is starting in the first one
 //  Returns `-1` if the second string is not in the first one
 
+int subString(string str, string keyword);
 
+int main() {
 
-int main(int argc, char** argv) {
-    /*// Example
-    int substr(char[] str, char[] keyword) {
+    char str[] = ("this is what I'm searching in");
+    char str2[] = ("searching");
 
-    return -1;
-    }
     // Should print: '17'
-    std::cout << substr("this is what I'm searching in", "searching")) << std::endl;
+    cout << subString("this is what I'm searching in", "searching") << endl;
 
     // Should print: '-1'
-    std::cout << substr("this is what I'm searching in", "not")) << std::endl;
-*/
-    char szo[] = 'sadadfdas';
-    std::cout << szo[3];
+    cout << subString("this is what I'm searching in", "not") << endl;
     return 0;
+}
+
+int subString(string str, string keyword) {
+    // Find first occurrence of 'g'
+    size_t found = str.find(keyword);
+    if (found != string::npos) {
+        return found;
+    } else return -1;
 }
