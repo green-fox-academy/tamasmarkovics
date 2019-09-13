5116e31abb8f7c88cc0a6c8b28ae64b9a28e0eb8
tamas.markovics <markovics.ttamas@gmail.com>
2019-09-13 13:08:59 +0200

    finish matrixDoubleColors

--- week-01/day-4/matrixDoubleColors/main.cpp
+++ week-01/day-4/matrixDoubleColors/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-#include <string>
+
+
 int main() {
     // - Create (dynamically) a two dimensional array
     //   with the following matrix. Use a loop!
@@ -15,6 +16,50 @@ int main() {
     int N = 1;
     std::cout << "how big?" << std::endl;
     std::cin >> N;
+    int matrix[N][N];
+    for (int i = 0; i < N; ++i) {
+        for (int j = 0; j < N; ++j) {
+            if (i == j) {
+                matrix[i][j]= 1 ;
+            }else matrix[i][j] = 0;
+        }
+    }
+    for (int i = 0; i < N; ++i) {
+        for (int j = 0; j < N; ++j) {
+            std::cout << matrix[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+
+
+    // - Create an array variable named `numList`
+    //   with the following content: `[3, 4, 5, 6, 7]`
+    // - Double all the values in the array
+    int numList[] = {3, 4, 5, 6, 7};
+    for (int k = 0; k < 5; ++k) {
+        std::cout << numList[k] << " -> ";
+        numList[k] *=2;
+        std::cout << numList[k] << std::endl;
+    }
+
+    // - Create a two dimensional array
+    //   which can contain the different shades of specified colors
+    // - In `colors[0]` store the shades of green:
+    //   `"lime", "forest green", "olive", "pale green", "spring green"`
+    // - In `colors[1]` store the shades of red:
+    //   `"orange red", "red", "tomato"`
+    // - In `colors[2]` store the shades of pink:
+    //   `"orchid", "violet", "pink", "hot pink"`
+    std::string colors[3][6] = {"green", "lime", "forest green", "olive", "pale green", "spring green", "red", "orange red", "red", "tomato", "%", "%", "pink", "orchid", "violet", "pink", "hot pink", "%"};
+    for (int l = 0; l < 3; ++l) {
+        std:: cout << colors[l][0] << ": ";
+        for (int i = 1; i < 6 && colors[l][i] != "%" ; ++i) {
+            std::cout << colors[l][i] << ", ";
+        }
+        std::cout << std::endl;
+    }
+
+
 
 
     return 0;
