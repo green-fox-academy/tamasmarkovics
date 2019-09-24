e17a39b8e42420334a40f9005c80fc4b3c675834
tamas.markovics <markovics.ttamas@gmail.com>
2019-09-24 16:02:45 +0200

    finish double

--- week-03/day-02/doubled/main.cpp
+++ week-03/day-02/doubled/main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+#include <sstream>
+#include <fstream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    // Create a program that decrypts the file called "duplicated-chars.txt",
+    // and pritns the decrypred text to the terminal window.
+
+    std::ifstream inputFile ("../double.txt");
+    std::string row;
+    while (std::getline(inputFile, row)) {
+        getline(inputFile, row);
+        for (int i = 0; i < row.size(); i++){
+            row.erase(i, 1);
+        }
+        std::cout << row << std::endl;
+    }
+
     return 0;
 }
