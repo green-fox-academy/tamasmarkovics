8bce291e8824c2255909b2c658a8c1e59f77e707
tamas.markovics <markovics.ttamas@gmail.com>
2019-10-02 13:48:23 +0200

    testing apples, anagram, sum

--- week-04/day-03/testSetup/MyApp_tests/apples.h
+++ week-04/day-03/testSetup/MyApp_tests/apples.h
@@ -2,10 +2,35 @@
 #define MYAPP_APPLES_H
 
 #include <string>
+#include <vector>
+using std::vector;
+using std::string;
 
 std::string getApple()
 {
     return "apple";
+}
+
+int sum(vector<int> toSum)
+{
+    int sum = 0;
+    for(int i = 0; i < toSum.size(); i++){
+        sum += toSum[i];
+    }
+    return sum;
+}
+
+
+bool anagram(string input1, string input2){
+    bool sofar;
+    if (input1.length() == input2.length()){
+        sofar = true;
+        for (int i = 0; ((i < input1.length()) && sofar); ++i) {
+            sofar = input1[i] == input2[input2.length() - 1 - i];;
+        }
+    } else sofar = false;
+
+    return sofar;
 }
 
 
--- week-04/day-03/testSetup/MyApp_tests/myAppTest.cpp
+++ week-04/day-03/testSetup/MyApp_tests/myAppTest.cpp
@@ -4,5 +4,42 @@
 
 TEST(apple_check, test_apple)
 {
+    std::cout << "Starting test apples()" << std::endl;
     EXPECT_EQ(getApple(), "apple");
 }
+
+TEST(SumCheck, testSumFullList)
+{
+    std::cout << "Starting test sum() with full list" << std::endl;
+    vector<int> toSum = {2,4,6,1,32,5,6,3,2};
+    ASSERT_EQ(sum(toSum), 61);
+}
+
+TEST(SumCheck, testSum0Elements)
+{
+    std::cout << "Starting test sum() with 0 elements" << std::endl;
+    vector<int> toSum = {};
+    ASSERT_EQ(sum(toSum), 0);
+}
+
+TEST(SumCheck, testSumOneElement)
+{
+    std::cout << "Starting test sum() with 1 element" << std::endl;
+    vector<int> toSum = {2};
+    ASSERT_EQ(sum(toSum), 2);
+}
+
+TEST(SumCheck, testSumNegativeElement)
+{
+    std::cout << "Starting test sum() with negative elements" << std::endl;
+    vector<int> toSum = {-2, -4, -120};
+    ASSERT_EQ(sum(toSum), -126);
+}
+
+
+TEST(AnagramCheck, testAnagram)
+{
+    std::cout << "Starting anagram check" << std::endl;
+    ASSERT_EQ(anagram("immawritesomethinglong", "gnolgnihtemosetirwammi"), true);
+}
+
